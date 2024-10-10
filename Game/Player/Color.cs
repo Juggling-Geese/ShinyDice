e07775d3c2f18270e738dee5489ec6ebfc15a7a4
Darin L <seekdtruth832@gmail.com>
2024-10-10 00:45:05 -0600

    Player

---
+++ Game/Player/Color.cs
@@ -0,0 +1,8 @@
+﻿namespace Game.Player
+{
+    public enum Color
+    {
+        White,
+        Black
+    }
+}
---
+++ Game/Player/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace Game.Player
+{
+    public class Player
+    {
+        public string Name { get; set; } = string.Empty;
+        public Color Color { get; set; } = Color.White;
+        public int VictoryPoints { get; set; } = 0;
+        public int Supplies { get; set; } = 0;
+    }
+}
