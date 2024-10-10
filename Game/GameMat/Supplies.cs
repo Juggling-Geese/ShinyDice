c2d1e83dac9eaa35a6bac3351e9c1f0d4c5b2c50
Darin L <seekdtruth832@gmail.com>
2024-10-10 13:21:07 -0600

    Create GameMatCrewContainer

--- Game/GameMat/Supplies.cs
+++ Game/GameMat/Supplies.cs
@@ -6,28 +6,32 @@ namespace Game.GameMat
     /// <summary>
     /// Represents the supplies in Serenity
     /// </summary>
-    public class Supplies()
+    public class Supplies() : GameMatCrewContainer
     {
-        private readonly List<CrewDie> _supplies = new List<CrewDie>();
+        private readonly CrewDice _crew = new CrewDice();
 
         public Supplies(CrewDice crew)
             : this()
         {
-            _supplies = crew.Supplies.ToList();
+            _crew = new CrewDice(
+                crew.Outlaws.Where(outlaw => outlaw.IsSupply()),
+                crew.Passengers.Where(passenger => passenger.IsSupply()));
         }
 
         /// <summary>
         /// The total number of supplies
         /// </summary>
-        public int Count => _supplies.Count;
+        public int Count => _crew.Supplies.Count();
 
         /// <summary>
         /// Adds a <see cref="CrewDie"/>, if die is a supply die
         /// </summary>
         /// <param name="dice">The given die to add to the supplies hold</param>
-        public void Add(params CrewDie[] dice)
+
+        public override void Add(params Die[] dice)
         {
-            _supplies.AddRange(dice.Where(die => die.IsSupply()));
+            var supplies = dice.Where(die => die is CrewDie crew && crew.IsSupply());
+            base.Add(supplies.ToArray());
         }
     }
 }
